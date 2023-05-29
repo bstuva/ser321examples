5ee31522ee77188395d4c3457db91f9cb14216db
Brayden <bdstuva@gmail.com>
2023-05-29 03:06:48 -0700

    removed todo message

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -281,7 +281,6 @@ class WebServer {
             builder.append("HTTP/1.1 200 OK\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
-            builder.append("Check the todos mentioned in the Java source file");
             // response based on what the assignment document asks for
 
               for(int i = 0; i < gatheredInfo.length(); i++){
@@ -295,6 +294,7 @@ class WebServer {
                 String resultsOwner = repoResults.getString("owner");
 
                 builder.append(resultsNAME + " " + resultsID + " " + resultsOwner);
+                builder.append("\n");
 
               }
 
