45ed5a17397b08a7fda3897951dec81d0db86ece
Brayden <bdstuva@gmail.com>
2023-05-29 02:39:02 -0700

    todo fix

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -284,7 +284,6 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Check the todos mentioned in the Java source file");
-            // TODO: Parse the JSON returned by your fetch and create an appropriate
             // response based on what the assignment document asks for
 
               for(int i = 0; i < gatheredInfo.length(); i++){
